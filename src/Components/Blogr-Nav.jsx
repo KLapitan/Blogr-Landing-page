a88ae161dd7710fc22962a58585ef217f9b3a116
KLapitan <karllapitan5@gmail.com>
2026-04-07 15:08:56 +0800

    details desktop version uidone

--- src/Components/Blogr-Nav.jsx
+++ src/Components/Blogr-Nav.jsx
@@ -6,6 +6,7 @@ const BlogrNav =({link}) => {
   const [showLinks,setShowLinks]=useState(false)
 
   const [activeLinks,setActiveLinks] =useState(null)
+  
 
 const handleShowLinks =() => {
 setShowLinks(prev => !prev)
@@ -32,15 +33,15 @@ useEffect(() => {
 }, [])
 
 return(
-<nav className="w-full border h-20 absolute top-12 z-30 p-2 lg:left-35 lg:w-6xl">
-<section className=" flex flex-row justify-between items-center m-3 relative ">
+<nav className="w-full flex border h-20 absolute top-12 z-30  lg:w-7xl lg:left-22 lg:shrink">
+<section className=" flex f-grow flex-row justify-between items-center m-3 border w-full">
 
-<div className="flex flex-row gap-10 items-center">
+<div className="flex flex-row gap-24 md:gap-10 lg:gap-17 items-center border">
 <picture>
 <img src="images/logo.svg" alt="logo-picture" />
 </picture>
 
-  <ul className=" flex-row gap-10 hidden lg:flex">
+  <ul className=" flex-row gap-10 hidden md:flex lg:flex">
   {link.map(item => (
   <li key={item.id} className="relative text-NWhite text-md font-Ubuntu font-bold">
   
@@ -73,9 +74,9 @@ return(
 </div>
 
 
-<div className="lg:flex flex-row gap-5 hidden ">
+<div className="md:flex lg:flex flex-row gap-7 hidden ">
 <button className="text-NWhite font-Ubuntu text-md">Login</button>
-<button className="bg-NWhite text-BGN-Red-550 font-Ubuntu font-bold px-8 py-3 text-md rounded-full text-center">Sign Up</button>
+<button className="bg-NWhite text-BGN-Red-550 font-Ubuntu font-bold px-10 py-3 text-md rounded-full text-center">Sign Up</button>
 
 
 </div>
